1da21c16959e5aaad58dc938835fb4d6a83097ad
Tanishq Yadav <ty8099@srmist.edu.in>
2026-02-23 11:16:10 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,23 +1,23 @@
 public class PalindromeCheckerApp {
 
     public static boolean isPalindrome(String input) {
-        // Convert string to character array
+       
         char[] chars = input.toCharArray();
 
-        // Initialize two pointers
+       
         int left = 0;
         int right = chars.length - 1;
 
-        // Compare characters from both ends
+      
         while (left < right) {
             if (chars[left] != chars[right]) {
-                return false; // Not a palindrome
+                return false; 
             }
             left++;
             right--;
         }
 
-        return true; // It is a palindrome
+        return true; 
     }
 
     public static void main(String[] args) {
