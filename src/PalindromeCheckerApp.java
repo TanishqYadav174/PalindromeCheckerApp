dca79a8aaab1d91ebb8ed5187a0b76f59055f41a
TanishqYadav174 <ty8099@srmist.edu.in>
2026-02-23 09:59:25 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,29 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-    public static void main (String args[]){}
+    public static void main(String[] args) {
 
-    System.out.print("Enter a String");
-    String word=
+        //UC1
+        displayWelcomeMessage();
+
+        //UC2
+        //eckHarcodedPallindrome();
+
+        //UC3
+        //eckPallindromeuseloop();
+
+        System.out.println("Application is ready for Pallindrome Processing");
+    }
+
+
+    //UC1 Logic
+    public static void displayWelcomeMessage() {
+        System.out.println("============================================");
+        System.out.println("           PALLINDROME CHECKER APP          ");
+        System.out.println("============================================");
+        System.out.println("Application Version : 1.0.0");
+        System.out.println("Developed for: Learning Git & Java");
+        System.out.println("=============================================");
+    }
 
 }
